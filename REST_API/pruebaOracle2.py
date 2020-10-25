fef35adb92a430144a25c4d475099f7ef11f975b
d4n11083 <d4n11083@estudiantec.cr>
2020-10-25 11:15:54 -0600

    Clase DB en pruebaOracle2.py,  pdf del proyecto, y cambios en REST_API

--- REST_API/pruebaOracle2.py
+++ REST_API/pruebaOracle2.py
@@ -1,12 +1,53 @@
 from __future__ import print_function 
-
+from flask import Flask
+from flask_restful import Api, Resource, reqparse
+import json
+import collections
 import cx_Oracle
 
-connection = cx_Oracle.connect("d4n11083/root@localhost/xe")
+app = Flask(__name__)
+api = Api(app)              #Nos indica que vamos a encerrar nuestra app como un REST API
 
-sql = "SELECT * FROM USUARIO"
 
-cursor = connection.cursor()
 
-for result in cursor.execute(sql):
-    print(result)
+
+class DataBase():
+    connection = cx_Oracle.connect("d4n11083/root@localhost/xe")
+    cursor = connection.cursor()
+
+    def getAll(self):
+        sql = "SELECT * FROM USUARIO"
+        self.cursor.execute(sql)
+        rows = self.cursor.fetchall()
+
+        objects_list = []
+        for row in rows:
+            d = collections.OrderedDict()
+            d["id"] = row[0]
+            d["name"] = row[1]
+            d["location"] = row[2]
+            objects_list.append(d)
+        j = json.dumps(objects_list)
+
+        return j
+    
+    def 
+
+db = DataBase()
+
+
+class HelloWorld(Resource):  #Maneja los request
+    def get(self):
+        return {"Usuarios": json.loads(db.getAll()) }
+    
+    def post(self):
+        return {"data":"posted"}
+
+api.add_resource(HelloWorld, "/helloworld")  #root del resource cuando enviamos un request
+
+
+
+
+
+if __name__ == "__main__":   #Inicializa el server en modo debug 
+    app.run(debug=True)     
---
+++ REST_API/readme.txt
@@ -0,0 +1,1 @@
+python pruebaOracle2.py 
